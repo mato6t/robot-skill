51540fe67baddee7747c80c92302f6e2c8121ce1
Mato6t <58171380+mato6t@users.noreply.github.com>
2020-06-07 20:40:24 +0200

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,15 +1,36 @@
 from mycroft import MycroftSkill, intent_file_handler
-
+import abb
 
 class Robot(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('robot.intent')
+    @intent_file_handler('robotl.intent')
     def handle_robot(self, message):
-        self.speak_dialog('robot')
-
+        self.speak_dialog('robotl')
+        robot=abb.YuMi("192.168.0.100")
+        robot.Connect()
+        robot.InitGrippers()
+        robot.MoveHome()
+        position=abb.JointTarget()
+        position.joints = [0,30,-60,-20,40,0,0,0,0,0,0,0]
+        position.speed = abb.Speed(100)
+        robot.LeftArm.MoveTo(position)
+        robot.MoveHome()
+    @intent_file_handler('robotr.intent')
+    def handle_robot(self, message):
+        self.speak_dialog('robotr')
+        robot=abb.YuMi("192.168.0.100")
+        robot.Connect()
+        robot.InitGrippers()
+        robot.MoveHome()
+        position=abb.JointTarget()
+        position.joints = [0,30,-60,-20,40,0,0,0,0,0,0,0]
+        position.speed = abb.Speed(100)
+        robot.RightArm.MoveTo(position)
+        robot.MoveHome()
 
 def create_skill():
     return Robot()
+
 
